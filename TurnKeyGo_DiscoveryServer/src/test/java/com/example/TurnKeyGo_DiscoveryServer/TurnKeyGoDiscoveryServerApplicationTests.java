1867920e607b72506b2e2ba0f6fbe06887d3127c
Vansh lute <vanshlute@gmail.com>
2026-09-15 12:56:34 +0530

    init

---
+++ TurnKeyGo_DiscoveryServer/src/test/java/com/example/TurnKeyGo_DiscoveryServer/TurnKeyGoDiscoveryServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.TurnKeyGo_DiscoveryServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootTest
+class TurnKeyGoDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
